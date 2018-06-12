7623afb5a6dcbc2c2ac262f59b1af3cd5e48bfbc
Fernando Gonzalez Chaio <34007887+fgchaio@users.noreply.github.com>
2018-06-12 15:38:23 -0300

    Update Ejemplo api php.php

--- Ejemplo api php.php
+++ Ejemplo api php.php
@@ -1,37 +1,34 @@
 <?php
-$apikey = "Api Key";
-$accountName = "Nombre de cuenta";
-$htmlCode = 'Tu codigo html aqui';
 
 function ObtainResult($options, $url, $returnId){
-		$context  = stream_context_create($options);
-		$result = file_get_contents($url, false, $context);
-		$headers = $http_response_header;
-		$matches = array();
-		preg_match('#HTTP/\d+\.\d+ (\d+)#', $headers[0], $matches);
-		$statusCode = intval($matches[1]);
-		if ($statusCode >= 200 && $statusCode < 300 && $returnId == true) {
-			$decodeResult = json_decode($result, true);
-			echo "Message : " . $decodeResult['message']  . "\r\n";
-			echo "id de creacion : " . $decodeResult['createdResourceId'];
-			return $decodeResult['createdResourceId'];
-		} else if ($statusCode >= 200 && $statusCode < 300 && $returnId == false){
-			$decodeResult = json_decode($result, true);
-			echo "Message : " . $decodeResult['message']  . "\r\n";
-		} else if ($statusCode >= 400) {
-			echo "\r\n**** ERROR ****\r\n";
-			echo "==== HEADERS ====";
-			var_dump($headers);
-			echo "==== BODY ====";
-			var_dump($result);
-		} else  {
-			echo "\r\n**** UNEXPECTED STATUS CODE ****\r\n";
-			echo "==== HEADERS ====";
-			var_dump($headers);
-			echo "==== BODY ====";
-			var_dump($result);
-		}
+	$context  = stream_context_create($options);
+	$result = file_get_contents($url, false, $context);
+	$headers = $http_response_header;
+	$matches = array();
+	preg_match('#HTTP/\d+\.\d+ (\d+)#', $headers[0], $matches);
+	$statusCode = intval($matches[1]);
+	if ($statusCode >= 200 && $statusCode < 300 && $returnId == true) {
+		$decodeResult = json_decode($result, true);
+		echo "Message : " . $decodeResult['message']  . "\r\n";
+		echo "id de creacion : " . $decodeResult['createdResourceId'];
+		return $decodeResult['createdResourceId'];
+	} else if ($statusCode >= 200 && $statusCode < 300 && $returnId == false){
+		$decodeResult = json_decode($result, true);
+		echo "Message : " . $decodeResult['message']  . "\r\n";
+	} else if ($statusCode >= 400) {
+		echo "\r\n**** ERROR ****\r\n";
+		echo "==== HEADERS ====";
+		var_dump($headers);
+		echo "==== BODY ====";
+		var_dump($result);
+	} else  {
+		echo "\r\n**** UNEXPECTED STATUS CODE ****\r\n";
+		echo "==== HEADERS ====";
+		var_dump($headers);
+		echo "==== BODY ====";
+		var_dump($result);
 	}
+}
 
 	
 function CreateList($apikey, $accountName, $listName){
@@ -53,18 +50,17 @@ function CreateList($apikey, $accountName, $listName){
 
 function SubscribersToList($apikey, $accountName, $listId, $subscriberEmail){
 	$url = "https://restapi.fromdoppler.com/accounts/" . $accountName . "/lists/" . $listId . "/subscribers"; // usar /accounts/$accountName/lists/$listId/subscribers/import en el caso de agregar mas de un subscriptor
-
 	$data = array(
 		'email' => $subscriberEmail
 	);
 
 	$options = array(
-	  'http' => array(
-		'header' => "Authorization: token " . $apikey . "\r\nContent-type: application/json\r\n",
-		'method' => 'POST',
-		'content' => json_encode($data),
-		'ignore_errors' => true
-	  )
+		'http' => array(
+			'header' => "Authorization: token " . $apikey . "\r\nContent-type: application/json\r\n",
+			'method' => 'POST',
+			'content' => json_encode($data),
+			'ignore_errors' => true
+		)
 	);
 	ObtainResult($options, $url, false);
 }
@@ -81,12 +77,12 @@ function CreateCampaign($apikey, $accountName, $campaignName, $campaignFromName,
 	);
 
 	$options = array(
-	  'http' => array(
-		'header' => "Authorization: token " . $apikey . "\r\nContent-type: application/json",
-		'method' => 'POST',
-		'content' => json_encode($data),
-		'ignore_errors' => true
-	  )
+		'http' => array(
+			'header' => "Authorization: token " . $apikey . "\r\nContent-type: application/json",
+			'method' => 'POST',
+			'content' => json_encode($data),
+			'ignore_errors' => true
+		)
 	);
 	$campaignId = ObtainResult($options, $url, true);
 	return $campaignId;
@@ -96,12 +92,12 @@ function ContentToCampaign($apikey, $accountName, $campaignId, $htmlCode){
 	$url = "https://restapi.fromdoppler.com/accounts/" . $accountName . "/campaigns/" . $campaignId . "/content";
 	$data = $htmlCode;
 	$options = array(
-	  'http' => array(
-		'header' => "Authorization: token " . $apikey . "\r\nContent-type: text/html\r\n",
-		'method' => 'PUT',
-		'content' => $data,
-		'ignore_errors' => true
-	  )
+		'http' => array(
+			'header' => "Authorization: token " . $apikey . "\r\nContent-type: text/html\r\n",
+			'method' => 'PUT',
+			'content' => $data,
+			'ignore_errors' => true
+		)
 	);
 	ObtainResult($options, $url, false);
 }
@@ -132,21 +128,28 @@ function SendCampaign ($apikey, $accountName, $campaignId){
 	);
 
 	$options = array(
-	  'http' => array(
-		'header' => "Authorization: token " . $apikey . "\r\nContent-type: application/json\r\n",
-		'method' => 'POST',
-		'content' => json_encode($data),
-		'ignore_errors' => true
-	  )
+		'http' => array(
+			'header' => "Authorization: token " . $apikey . "\r\nContent-type: application/json\r\n",
+			'method' => 'POST',
+			'content' => json_encode($data),
+			'ignore_errors' => true
+		)
 	);
 	ObtainResult ($options, $url, false);
 }
 
+$apikey = 'Api Key';
+$accountName = 'Nombre de cuenta';
+$htmlCode = 'Tu codigo html aqui';
+$Subscriber = 'Email del subscriptor';
+$fromEmail = 'Email del remitente';
+$replyToEmail = 'Email de respuesta';
+
 $listId = CreateList($apikey, $accountName, "lista creada por api");
 
-SubscribersToList($apikey, $accountName, $listId, "email subscriptor");
+SubscribersToList($apikey, $accountName, $listId, $Subscriber);
 
-$campaignId = CreateCampaign($apikey, $accountName, 'Nombre de campaña', 'Nombre remitente', 'From Email', 'Asunto de campaña', 'Preencabezado campaña', 'Email remitente');
+$campaignId = CreateCampaign($apikey, $accountName, 'Nombre de campaña', 'Nombre remitente', $fromEmail, 'Asunto de campaña', 'Preencabezado campaña', $replyToEmail);
 
 ContentToCampaign($apikey, $accountName, $campaignId, $htmlCode);
 
